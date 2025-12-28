bff7f9caf24ac1a0e3e55d92123394ca8f0bcb5f
ibrahimgencc <ibrahimgenc1515@gmail.com>
2025-12-28 21:36:51 +0300

    Update slln_simulation.py
    This script simulates the Strong Law of Large Numbers by computing the cumulative mean of i.i.d. U(0,1) random variables and visualizing its convergence to the expected value.

--- src/slln_simulation.py
+++ src/slln_simulation.py
@@ -1,1 +1,18 @@
-# SLLN simulation
+import numpy as np
+import matplotlib.pyplot as plt
+
+X = np.random.uniform(0, 1, n)
+
+S_n = np.cumsum(X) / np.arange(1, n + 1)
+
+plt.figure(figsize=(10, 6))
+plt.plot(S_n, label='Cumulative Mean $S_n$')
+plt.axhline(y=0.5, color='red', linestyle='--', label='μ = 0.5')
+
+plt.xlabel('Number of Observations (n)')
+plt.ylabel('Cumulative Mean')
+plt.title('SLLN Simulation for U(0,1)')
+plt.legend()
+plt.grid(True)
+plt.show()
+
